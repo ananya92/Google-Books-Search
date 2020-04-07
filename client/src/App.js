c83ad59f3dd7073c2f0346849363b48aa3f33e5b
ananya92 <1992ananya@gmail.com>
2020-04-08 07:41:38 +0930

    Added jumbotron heading

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Layout, Header, Navigation, Content} from 'react-mdl';
 import NavTabs from "./components/navTabs";
 import {Link} from 'react-router-dom';
+import {Jumbotron} from 'react-bootstrap';
 import "./App.css";
 
 function App() {
@@ -14,6 +15,14 @@ function App() {
                     <Link to="/saved">Saved</Link>
                 </Navigation>
             </Header>
+            <Jumbotron>
+                <div className="alignCenter">
+                    <h3>Google Books Search</h3>
+                    <p>
+                        Search and save books of interest!
+                    </p>
+                </div>
+                </Jumbotron>
             <Content>
                 <div className="page-content" />
                 <NavTabs></NavTabs>
