0d29d95ddaf5dc0fa5a6ed63a5321d44ce1654de
ananya92 <1992ananya@gmail.com>
2020-04-08 16:46:12 +0930

    Coded favorite book saving to database

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,11 +1,13 @@
 import React, {useRef, useState, useEffect} from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import API from "../utils/API";
-
 function Search() {
     const searchRef = useRef();
     const [searchedBooks, setSearchedBooks] = useState({
         books: []
+    });
+    const [favouriteBook, setFavouriteBook] = useState({
+        favbook: ""
     });
     const handleSubmit = e => {
         e.preventDefault();
@@ -14,7 +16,39 @@ function Search() {
     useEffect(()=>{
         searchRef.current.value = "Harry Potter";
         searchBooks();
-    },[])
+    },[]);
+
+    useEffect(() => {
+        if(favouriteBook.favbook !== "") {
+            var book = favouriteBook.favbook;
+            var isFavorite = false;
+            API.getFavBooks().then(result => {
+                for(var i=0; i< result.data.length; i++) {
+                    if(result.data[i].authors === book.volumeInfo.authors && result.data[i].title === book.volumeInfo.title) {
+                        console.log("Book is already a favorite");
+                        isFavorite = true;
+                        break;
+                    }
+                }
+                if(!isFavorite) {
+                    var desc = book.volumeInfo.description !== undefined ? book.volumeInfo.description : "";
+                    var imageSrc = book.volumeInfo.imageLinks !== undefined ? book.volumeInfo.imageLinks.smallThumbnail : "default.png";
+                    API.saveBook({
+                        authors: book.volumeInfo.authors,
+                        description: desc,
+                        image: imageSrc,
+                        link: book.volumeInfo.infoLink,
+                        title: book.volumeInfo.title,
+                    })
+                    .then(result => {
+                        console.log(result.data);
+                    })
+                    .catch(err => console.log(err));
+                }
+            })
+            .catch(err => console.log(err));
+        }
+    }, [favouriteBook]);
 
     function searchBooks() {
         API.getBooks(searchRef.current.value)
@@ -26,6 +60,9 @@ function Search() {
                 setSearchedBooks({books: result.data.items});
         })
         .catch(err => console.log(err));
+    }
+    function handleFavouriteChange(book) {
+        setFavouriteBook({favbook: book});
     }
     return(
         <div>
@@ -49,6 +86,9 @@ function Search() {
                         searchedBooks.books.map(book => (
                             <Col className="styleBook">
                                 <h5>{book.volumeInfo.title}</h5>
+                                <span className="favButton blueHeart" onClick = {() => handleFavouriteChange(book)}>
+                                    <i class="fa fa-heart"></i>
+                                </span>
                                 <p>Author: {book.volumeInfo.authors.join(", ")}</p>
                                 <div className="styleGrid"> 
                                         {(book.volumeInfo.imageLinks === undefined) ? 
@@ -59,6 +99,9 @@ function Search() {
                                         <p>{book.volumeInfo.description}</p>   
                                     </div>
                                 </div>  
+                                <button className="viewButton">
+                                    <a href={book.volumeInfo.infoLink} target="_blank">Read more</a>
+                                </button>
                             </Col>
                         ))
                     ) : (
